a7318fc058250478c12e0b411c6703757f3e6041
jaspkkr <141145910+jaspkkr@users.noreply.github.com>
2024-04-23 18:00:25 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,82 @@
+public class Main {
+    static class MyNode{
+        private int id;
+        private MyNode node;
+
+
+        public MyNode getNext() {
+            return node;
+        }
+
+        public MyNode(int id, MyNode node) {
+            this.id = id;
+            this.node = node;
+        }
+
+        public void setNode(MyNode node) {
+            this.node = node;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public String toString() {
+            return "MyNode{" +
+                    "id=" + id +
+                    ", node=" + node +
+                    '}';
+        }
+    }
+    public static void main(String[] args) {
+
+        MyNode list = new MyNode(1, new MyNode(2, new MyNode(3, null)));
+
+        System.out.println("Actual List:");
+        printList(list);
+
+        System.out.println("Reversed List: " + reverse(list));
+    }
+
+    private static MyNode reverse(MyNode list) {
+        MyNode start = list;
+        MyNode finalList =  null;
+
+        while (start.getNext() != null){
+            if (finalList ==null)
+                finalList = new MyNode(start.getId(), null);
+            else
+                finalList = new MyNode(start.getId(), finalList);
+
+            start = start.getNext();
+        }
+
+        //last node will never be processed by above loop
+        // because it's node element is null so loop makes an exit before processing last node
+        // so we need this line as a workaround.
+        start.setNode(finalList);
+
+        return start;
+    }
+
+
+
+    //If I recall correctly this was the existing printList method in the coding challenge.
+    private static void printList(MyNode list) {
+        MyNode start = list;
+
+        while (start.getNext() != null){
+            System.out.println(start.getId());
+            start = start.getNext();
+        }
+
+        //I don't recall completly if original printList method had code to print last node, but I never saw 3 getting printed
+        //so if not, we need to add an extra line here to print last node for the same reason
+        //as described at line 68.
+    }
+}
